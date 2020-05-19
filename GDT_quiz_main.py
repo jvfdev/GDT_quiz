70f75ef5284f1e123dc653e2b5e994b9fe039375
jvfdev <38409266+jvfdev@users.noreply.github.com>
2020-05-18 21:19:14 -0400

    Added Tkinter frame

--- GDT_quiz_main.py
+++ GDT_quiz_main.py
@@ -1,16 +1,41 @@
 from pathlib import Path
 import os
+from tkinter import *
+from PIL import Image, ImageTk
 
-working_directory = Path.cwd()
+# working_directory = Path.cwd()
 
-gdt_flatness = {'name':'flatness',
-                'image_file_name': 'flatness.png',
-                'Type_of_tolerance': 'Form',
-                'datum_required': "No"}
+# gdt_flatness = {'name':'flatness',
+#                 'image_file_name': 'flatness.png',
+#                 'Type_of_tolerance': 'Form',
+#                 'datum_required': "No"}
 
-gdt_straightness = {'name':'straightness',
-                'image_file_name': 'straightness.png',
-                'Type_of_tolerance': 'Form',
-                'datum_required': "No"}
+# gdt_straightness = {'name':'straightness',
+#                 'image_file_name': 'straightness.png',
+#                 'Type_of_tolerance': 'Form',
+#                 'datum_required': "No"}
 
-print('done')
+class Window(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.master = master
+        self.init_window()
+
+    def init_window(self):
+        self.master.title("GD&T Symbol Quiz")
+        self.pack(fill=BOTH, expand=1)
+
+        gdt_menu = Menu(self.master)
+        self.master.config(menu=gdt_menu)
+
+        file_menu = Menu(gdt_menu)
+        file_menu.add_command(label="Exit", command=self.client_exit)
+        gdt_menu.add_cascade(label="File", menu=file_menu)
+
+    def client_exit(self):
+        root.quit()
+
+root = Tk()
+root.geometry("800x600")
+app = Window(root)
+root.mainloop()
